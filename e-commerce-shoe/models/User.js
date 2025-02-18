c0e6c3f4b621d85df062d7f62ba8867a13c18c99
ThunderRabbit <zuzazibubabi@gmail.com>
2025-02-18 23:45:32 +0800

    Remove all content

--- e-commerce-shoe/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-});
-
-export default mongoose.models.User || mongoose.model("User", UserSchema);
