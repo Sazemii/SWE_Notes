6d02cccbab581c55dc4a4ce153340beba6b0c765
ThunderRabbit <zuzazibubabi@gmail.com>
2026-03-02 18:59:35 +0800

    added arduino notes

---
+++ SWE/midtermphys/code.ino
@@ -0,0 +1,7 @@
+void setup() {
+    // random seed for random games or else it will 
+  // Set all 7 segment output pins to OUTPUT and turn them off
+  // Set all buttons as INPUT_PULLUP (button press = LOW signal) 
+  // Set LED pins as outputs and ensure they start off
+    
+}
